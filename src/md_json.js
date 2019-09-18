26a0f1456bc7554df21c490d9223702babbde434
Lars Trieloff <trieloff@adobe.com>
2019-09-18 08:13:43 +0000

    feat(index): create index fragments for sections, images, paragraphs

--- src/md_json.js
+++ src/md_json.js
@@ -9,6 +9,24 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+const select = require('unist-util-select');
+const string = require('mdast-util-to-string');
+
+function indexnode(root, pattern, props) {
+  return select.selectAll(pattern, root).map((node, index) => {
+    return Object.keys(props).reduce((retval, propname) => {
+      const funcorval = props[propname];
+      if (typeof funcorval === 'function') {
+        retval[propname] = props[propname](node);
+      } else {
+        retval[propname] = funcorval;
+      }
+      return retval;
+    }, {
+      fragmentId: `${pattern}[${index}]`
+    });
+  })
+}
 
 /**
  * The 'pre' function that is executed before the HTML is rendered
@@ -16,13 +34,41 @@
  * @param context.content The content
  */
 async function main(context) {
-  const meta = context.content.meta;
+  const full = context.content.mdast;
   const docs = [];
+
+  // build an extra index of sections
+  docs.push(...indexnode(full, 'section', {
+    type: ({type}) => type,
+    image: ({image}) => image,
+    value: ({intro}) => intro,
+    title: ({title}) => title,
+    types: ({meta}) => meta.types
+  }));
+
+  // build an extra index of images linked from text
+  docs.push(...indexnode(full, 'image', {
+    type: 'imageref',
+    image: ({url}) => url,
+    value: ({alt}) => alt,
+    title: ({title}) => title
+  }));
+
+  // build an extra index of full text paragraphs
+  docs.push(...indexnode(full, 'paragraph, heading', {
+    type: 'fulltext',
+    value: (node) => string(node),
+  }));
 
   return {
     response: {
       body: {
-        meta,
+        meta: {
+          ...(full.meta || select.select('section', full).meta),
+          title: full.title || select.select('section', full).title,
+          value: full.intro || select.select('section', full).intro,
+          sections: select.selectAll('section', full).length
+        },
         docs
       },
     },
